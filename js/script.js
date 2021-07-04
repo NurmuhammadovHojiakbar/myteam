a9c647c4a08755866bd36c83b416935c4c85b405
NurmuhammadovHojiakbar <pythoncoderuz@gmail.com>
2021-07-04 11:57:20 +0500

    directors section

--- js/script.js
+++ js/script.js
@@ -9,3 +9,27 @@ elOpen.addEventListener("click", function(){
 elClose.addEventListener("click", function(){
     elSiteNav.style.right = "-100%"
 })
+
+
+
+const elPlusButton = document.querySelectorAll(".director-button");
+const elDirectorInfo = document.querySelectorAll(".director-info");
+
+if(elPlusButton.length > 0){
+    elPlusButton.forEach(function(plus){
+        plus.addEventListener("click", function(){
+            elPlusButton.forEach(function(item){
+                item.classList.remove("director-button__focus")
+             })
+            elDirectorInfo.forEach(function(off){
+                off.classList.remove("director-info__active")
+             })
+    
+            plus.classList.add("director-button__focus")
+                
+               
+    
+        })
+    })
+    
+}
